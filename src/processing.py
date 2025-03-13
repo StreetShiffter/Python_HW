8620ade9a2d90f0f7534402fd4a99d2850b662e2
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-03-14 00:12:54 +0700

    Fix file processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(list_data: list[dict], status: str) -> list[dict[str, str | int]]:
+def filter_by_state(list_data: list[dict], state = 'EXECUTED') -> list[dict[str, str | int]]:
     """Функция сортировки словарей по значению"""
     list_dict_check = []
     for item in list_data:
-        if item.get("state") == status:
+        if item.get("state") == state:
             list_dict_check.append(item)
 
     return list_dict_check
@@ -12,25 +12,23 @@ def filter_by_state(list_data: list[dict], status: str) -> list[dict[str, str | 
 #     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
 #     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
 #     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},ы
+#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 # ]
-# check_word = "EXECUTED"
-# result = filter_by_state(test, check_word)
+# result = filter_by_state(test)
 # print(result)
 
 
-def sort_by_date(list_check_date: list[dict], value_sort: bool) -> list[dict]:
+def sort_by_date(list_check_date: list[dict], value_sort = True) -> list[dict]:
     """Функция сортировки даты"""
-    sorting = sorted(list_check_date, key=lambda x: x["date"], reverse=value_sort)
+    sorting = sorted(list_check_date, key=lambda x: x["date"], reverse = not value_sort)
     return sorting
 
 
-# test_data = [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-# sorting_value = True
-# result_data = sort_by_date(test_data, sorting_value)
-# print(result_data)
+test_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+result_data = sort_by_date(test_data)
+print(result_data)
