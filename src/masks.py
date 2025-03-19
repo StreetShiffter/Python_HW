a144e8f4cb20a0a90d91f3b4fe1433274c610208
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-03-19 22:47:07 +0700

    Реализовано тестирование модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,18 +1,33 @@
 from typing import Union
+import pytest
 
 
 def get_mask_card_number(number_card: Union[int, str]) -> str:
     """Функция, которая маскирует предпоследние 6 цифр"""
+    card_number = str(number_card).replace(" ", "")
+    if len(card_number) < 16 or len(card_number) > 16: # Проверка на длину
+        raise ValueError("Введите ровно 16 символов карты!")
+    if not card_number.isdigit(): #Проверка на числовое значение
+        raise ValueError("Введите числовое значение")
 
-    card_number = str(number_card)
 
-    return f"{card_number[:4]} {card_number[4:6]} {'**'} {'****'} {card_number[-4:]}"
+    return f"{card_number[:4]} {card_number[4:6]}{'**'} {'****'} {card_number[-4:]}"
 
 
 def get_mask_account(number_account: Union[int, str]) -> str:
     """Функция для маскирования номера аккаунта, оставляя 4 последние цифры"""
-
     account_number = str(number_account)
     visible_numbers = account_number[-4:]
+    if not visible_numbers.isdigit(): #Проверка на числовое значение
+        raise ValueError("Введите числовое значение")
+    if len(visible_numbers) < 4: # Проверка на длину
+        raise ValueError("Короткий номер")
+
 
     return f"** {visible_numbers}"
+
+test = "1234 5678 9876 5435"
+result = get_mask_account(test)
+print(result)
+
+
---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+@pytest.fixture
+def info_string():
+    return "1234 5678 9876 5432"
