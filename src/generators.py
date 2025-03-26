a3f2eb907c19ff30d0193adab784fc8dd4007c71
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-03-26 20:14:11 +0700

    Cоздан модуль generators

---
+++ src/generators.py
@@ -0,0 +1,8 @@
+def filter_by_currency(transaktions: list[dict])-> Generator:
+    pass
+
+def transaction_descriptions(start: int, end: int)-> Generator:
+    pass
+
+def card_number_generator():
+    pass
