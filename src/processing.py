1e2b505c30be695f59b5217cf18d414b87f67df3
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-03-28 01:18:33 +0700

    Исправления в файле README.md

--- src/processing.py
+++ src/processing.py
@@ -8,18 +8,6 @@ def filter_by_state(list_data: list[dict[str, str]], state: str = "EXECUTED") ->
     return list_dict_check
 
 
-#
-
-# test = [
-#     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#     {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-# ]
-# result = filter_by_state(test)
-# print(result)
-
-
 def sort_by_date(list_check_date: list[dict], value_sort: bool = True) -> list[dict]:
     """Функция сортировки даты"""
     for item in list_check_date:
@@ -29,10 +17,3 @@ def sort_by_date(list_check_date: list[dict], value_sort: bool = True) -> list[d
             raise TypeError("Значение даты должно быть числовым значением")
     sorting = sorted(list_check_date, key=lambda x: x["date"], reverse=value_sort)
     return sorting
-
-
-# test_data = [
-#     {"id": 1, "state": "EXECUTED", "date": 'j'},
-# ]
-# result_data = sort_by_date(test_data)
-# print(result_data)
