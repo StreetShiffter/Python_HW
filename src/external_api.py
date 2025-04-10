05c8c8ca3fc4f8530f3722520d7c47b212191ff0
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-04-10 20:19:35 +0700

    Обновление вывода данных через float

--- src/external_api.py
+++ src/external_api.py
@@ -9,7 +9,7 @@ API_KEY = os.getenv("API")
 MY_KEY_API = "API"  # Не забудьте вставить свой API ключ'
 
 
-def currency_exchange(output_code: str, input_code: str, amount: int | float) -> list[dict]:
+def currency_exchange(output_code: str, input_code: str, amount: int | float) -> dict:
     """Функция конвертации любой валюты"""
     url = f"https://api.apilayer.com/exchangerates_data/convert?to={input_code}&from={output_code}&amount={amount}"
 
@@ -23,11 +23,11 @@ def currency_exchange(output_code: str, input_code: str, amount: int | float) ->
         response.raise_for_status()  # Генерируем исключение для ошибок
 
 
-def transaction_info(transaction: list[dict]) -> any:
+def transaction_info(transaction: list[dict]) -> float:
     """Вывод суммы транзакции"""
     for item in transaction:
         if item["operationAmount"]["currency"]["code"] == "RUB":
-            money = item["operationAmount"]["amount"]
+            money = float(item["operationAmount"]["amount"])
             return money
 
         elif item["operationAmount"]["currency"]["code"] != "RUB":
@@ -37,28 +37,28 @@ def transaction_info(transaction: list[dict]) -> any:
             result_dict = currency_exchange(input, output, convert)
             money = result_dict["result"]
 
-            return money
+            return float(money)
         else:
-            return f"Валюта не определена"
+            return 0.0
 
 
-# transaction = [
-#
-# {
-#     "id": 41428829,
-#     "state": "EXECUTED",
-#     "date": "2019-07-03T18:35:29.512364",
-#     "operationAmount": {
-#       "amount": "8221.37",
-#       "currency": {
-#         "name": "AED",
-#         "code": "AED"
-#       }
-#     },
-#     "description": "Перевод организации",
-#     "from": "MasterCard 7158300734726758",
-#     "to": "Счет 35383033474447895560"
-#   }]
-#
-# result=transaction_info(transaction)
-# print(result)
+transaction = [
+
+{
+    "id": 41428829,
+    "state": "EXECUTED",
+    "date": "2019-07-03T18:35:29.512364",
+    "operationAmount": {
+      "amount": "8221.37",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+  }]
+
+result=transaction_info(transaction)
+print(type(result))
