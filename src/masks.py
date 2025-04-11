f93308a50c44c6a8a7eb8c1bf30f94954ea82e7a
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-04-12 00:49:46 +0700

    Реализовано логирование модуля masks

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,37 @@
 from typing import Union
-# from decorators import log
-#
-# @log(filename="mylog.txt")
+import logging
+
+loger_func = logging.getLogger(__name__) # логер к текущему модулю
+file_handler = logging.FileHandler('../logs/example.log', encoding = 'utf-8')
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+loger_func.addHandler(file_handler)
+loger_func.setLevel(logging.DEBUG)
+
+
+
+
 def get_mask_card_number(number_card: Union[int, str]) -> str:
     """Функция, которая маскирует предпоследние 6 цифр"""
     card_number = str(number_card).replace(" ", "")
     if not card_number.isdigit():  # Проверка на числовое значение
+        loger_func.error("Введено нечисловое значение: %s", number_card)
         raise ValueError("Введите числовое значение")
 
     if len(card_number) == 16:  # Маскировка для 16 символов
-        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+        masked_number =  f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+        loger_func.info("Успешная маскировка номера карты: %s", masked_number)
+        return masked_number
 
     elif len(card_number) == 21:  # Маскировка для 21 символа
-        return f"{card_number[:11]}******{card_number[-4:]}"
+        masked_number = f"{card_number[:11]}******{card_number[-4:]}"
+        loger_func.info("Успешная маскировка номера карты: %s", masked_number)
+        return masked_number
 
+    loger_func.error("Неверный номер карты! Введено: %s", number_card)
     raise ValueError("Неверный номер карты! Введите 16 или 21 символ.")
 
-# test = "1234589674125896"
+# test = 5
 # get_mask_card_number(test)
 
 
@@ -25,13 +40,17 @@ def get_mask_account(number_account: Union[int, str]) -> str:
     account_number = str(number_account)
     visible_numbers = account_number[-4:]
     if not visible_numbers.isdigit():  # Проверка на числовое значение
+        loger_func.error("Введено нечисловое значение: %s", number_account)
         raise ValueError("Введите числовое значение")
     if len(visible_numbers) < 4:  # Проверка на длину
+        loger_func.error("Не корректная длина номера: %s", number_account)
         raise ValueError("Короткий номер")
 
-    return f"** {visible_numbers}"
+    masked_account_number = f"** {visible_numbers}"
+    loger_func.info("Успешная маскировка номера карты: %s", number_account)
+    return masked_account_number
 
 
-# test = "1234 5678 9876 5435"
-# result = get_mask_card_number(test)
-# print(result)
+test = "o"
+get_mask_card_number(test)
+
