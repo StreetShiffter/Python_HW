c7677e2da6753468a8b4bdbd73c35b810e4ac485
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-04-16 20:23:39 +0700

    Fix README

--- src/open_file_stat.py
+++ src/open_file_stat.py
@@ -8,26 +8,20 @@ file_csv = os.path.join(script_dir, "../data/transactions.csv")
 file_excel = os.path.join(script_dir, "../data/transactions_excel.xlsx")
 
 
-
-def open_csv(filename: str, delimiter=',') -> List[List[Any]]:
-    '''Функция читающая CSV файл'''
+def open_csv(filename: str, delimiter: str = ',') -> List[dict[str, Any]]:
+    """Функция читающая CSV файл."""
     try:
-        with open(filename, 'r',  encoding = 'utf-8') as file:
-            reader = list(csv.DictReader(file, delimiter=delimiter))
-            return reader
-
+        with open(filename, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=delimiter)
+            return list(reader)
     except Exception as error:
-        raise  Exception(f'Ошибка при чтении файла: {str(error)}')
+        raise Exception(f'Ошибка при чтении файла: {str(error)}')
 
-def open_excel(filename:str) -> List[List[Any]]:
-    '''Функция читающая excel файл'''
+
+def open_excel(filename: str) -> List[dict[Any, Any]]:
+    """Функция читающая excel файл."""
     try:
         df = pd.read_excel(filename)
         return df.to_dict('records')
     except Exception as error:
-        raise  Exception(f'Ошибка при чтении файла: {str(error)}')
-
-
-#total = open_csv(file_csv, ':')
-total = open_excel(file_excel)
-print(total)
+        raise Exception(f'Ошибка при чтении файла: {str(error)}')
