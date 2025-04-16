4add4da13a95a1c8d629f3eedda4c1b376fdbfb2
Андрей Гордиевский <agwzdushaz@gmail.com>
2025-04-16 10:23:53 +0700

    Реализована функция open_csv

--- src/open_file_stat.py
+++ src/open_file_stat.py
@@ -2,7 +2,17 @@ import csv
 import os
 import pandas as pd
 
-
 script_dir = os.path.dirname(os.path.abspath(__file__))
 file_csv = os.path.join(script_dir, "../data/transactions.csv")
 file_excel = os.path.join(script_dir, "../data/transactions_excel.xlsx")
+
+
+def open_csv(filename: str, delimiter=',') -> str:
+    with open(filename, 'r',  encoding = 'utf-8') as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        row = [item for item in reader]
+    return row
+
+
+total = open_csv(file_csv, ':')
+print(total)
